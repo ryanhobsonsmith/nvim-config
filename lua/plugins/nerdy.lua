4ba04ddd0884ea6ff3715400989c56a1dbdea0c2
Ryan Smith <ryanhobsonsmith@gmail.com>
2026-04-04 13:15:35 -0700

    fix nerdy config

--- lua/plugins/nerdy.lua
+++ lua/plugins/nerdy.lua
@@ -1,6 +1,7 @@
 return {
   "2kabhishek/nerdy.nvim",
   dependencies = { "folke/snacks.nvim" },
+  config = true,
   cmd = "Nerdy",
   keys = {
     { "<leader>in", "<cmd>Nerdy<cr>", desc = "Nerd Icons" },
