b6479f00bc109466c69f7ceb5162d1bb5cb55958
Ryan Smith <ryanhobsonsmith@gmail.com>
2026-04-18 22:32:56 -0700

    disable lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,23 +1,24 @@
-return {
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      opts.sections.lualine_z = {
-        function()
-          return " " .. os.date("%I:%M %p")
-        end,
-      }
-      opts.winbar = {
-        lualine_c = {
-          { "filename", path = 1, color = { fg = "#98c379", gui = "bold" } },
-        },
-      }
-      opts.inactive_winbar = {
-        lualine_c = {
-          { "filename", path = 1, color = { fg = "#61afef" } },
-        },
-      }
-    end,
-  },
-}
+return {}
+-- return {
+--   {
+--     "nvim-lualine/lualine.nvim",
+--     event = "VeryLazy",
+--     opts = function(_, opts)
+--       opts.sections.lualine_z = {
+--         function()
+--           return " " .. os.date("%I:%M %p")
+--         end,
+--       }
+--       opts.winbar = {
+--         lualine_c = {
+--           { "filename", path = 1, color = { fg = "#98c379", gui = "bold" } },
+--         },
+--       }
+--       opts.inactive_winbar = {
+--         lualine_c = {
+--           { "filename", path = 1, color = { fg = "#61afef" } },
+--         },
+--       }
+--     end,
+--   },
+-- }
