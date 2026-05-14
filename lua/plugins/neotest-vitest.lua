f4b788d1ddb2212fd47bfb09f3b610a9e9ee5a2a
Ryan Smith <ryanhobsonsmith@gmail.com>
2026-05-14 13:34:50 -0700

    save before running test

--- lua/plugins/neotest-vitest.lua
+++ lua/plugins/neotest-vitest.lua
@@ -1,3 +1,13 @@
+-- Save all modified buffers before running tests. Without this, it's easy to
+-- run a test against the on-disk version of a file you've been editing and get
+-- a confusing failure that doesn't match what's on screen.
+local function save_and_run(fn)
+  return function()
+    vim.cmd("silent! wall")
+    fn()
+  end
+end
+
 return {
   {
     "nvim-neotest/neotest",
@@ -11,6 +21,14 @@ return {
           end,
         },
       },
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>tt", save_and_run(function() require("neotest").run.run(vim.fn.expand("%")) end), desc = "Run File (Neotest)" },
+      { "<leader>tT", save_and_run(function() require("neotest").run.run(vim.uv.cwd()) end), desc = "Run All Test Files (Neotest)" },
+      { "<leader>tr", save_and_run(function() require("neotest").run.run() end), desc = "Run Nearest (Neotest)" },
+      { "<leader>tl", save_and_run(function() require("neotest").run.run_last() end), desc = "Run Last (Neotest)" },
+      { "<leader>td", save_and_run(function() require("neotest").run.run({ strategy = "dap" }) end), desc = "Debug Nearest (Neotest)" },
     },
   },
 }
